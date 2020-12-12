5a2c34cb4f579dd965633bb43b5f66d843f811cf
joannes <joannesperret@hotmail.fr>
2020-12-12 22:12:10 +0100

    Modification du bouton accueil sur page produits et shoping-card

--- boundaries/shoping-cart.php
+++ boundaries/shoping-cart.php
@@ -97,20 +97,18 @@
             </div>
             <div class="row">
                 <div class="col-lg-12">
-                    <div class="shoping__cart__btns">
-                        <a href="index.php?action=accueil#catalogue" class="primary-btn cart-btn">CONTINUE SHOPPING</a>
-                        <a href="index.php?action=shoping-cart#panier" class="primary-btn cart-btn cart-btn-right"><span class="icon_loading"></span>
-                            Mettre à jour</a>
+                    <div class="shoping__cart">
+                        <a href="index.php?action=accueil#catalogue" class="primary-btn cart-btn">POURSUIVRE VOS ACHATS</a>                       
                     </div>
                 </div>
                 <div class="col-lg-6">
                     <div class="shoping__continue">
                         <div class="shoping__discount">
-                            <h5>Discount Codes</h5>
+<!--                            <h5>Discount Codes</h5>
                             <form action="#">
                                 <input type="text" placeholder="Enter your coupon code">
                                 <button type="submit" class="site-btn">AJOUTER UN COUPON</button>
-                            </form>
+                            </form>-->
                         </div>
                     </div>
                 </div>
